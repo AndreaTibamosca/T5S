be2016a47105825d51c27bcb7f820112bce38905
AndreaTibamosca <adgonzalezt@itc.edu.co>
2023-08-27 19:55:13 -0500

    controladores

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.EntityFrameworkCore;
+using T5S.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<T5sContext>(options => options.UseSqlServer("Server=DESKTOP-QO3RBTB;Database=T5S; Trusted_Connection=true;MultipleActiveResultSets=true"));
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
