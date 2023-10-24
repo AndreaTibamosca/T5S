6cd3a6f8ed8a8dcdfe51be843db725d73cbbf994
AndreaTibamosca <adgonzalezt@itc.edu.co>
2023-10-23 20:59:12 -0500

    Ultimo

---
+++ ModelViews/LoginMV.cs
@@ -0,0 +1,17 @@
+﻿namespace T5S.ModelViews
+{
+    public class LoginMV
+    {
+
+        public string Nombreusuario { get; set; }
+
+        public string Password { get; set; }
+
+
+        public string Nombre { get; set; } = null!;
+
+
+
+
+    }
+}
