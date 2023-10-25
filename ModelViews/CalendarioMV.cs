3508c800aadc27dea35bc76d2d45ac7aa3b5862c
DfGarcia21 <dfgarciab@itc.edu.co>
2023-10-25 09:54:19 -0500

    Actualizacion a los controladores de calendario y forma de pago y a los modelos: calendarioMV y FormaPagoMV

---
+++ ModelViews/CalendarioMV.cs
@@ -0,0 +1,16 @@
+﻿namespace T5S.MoldelsView
+{
+    public class CalendarioMV
+    {
+        public int Id { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Descripcion { get; set; } = null!;
+
+        public string Barrio { get; set; } = null!;
+
+        public string TipoTutoria { get; set; } = null!;
+
+    }
+}
---
+++ ModelViews/FormaPagoMV.cs
@@ -0,0 +1,17 @@
+﻿namespace T5S.MoldelsView
+{
+    public class FormaPagoMV
+    {
+
+        public int Id { get; set; }
+
+        public string TipoPago { get; set; }
+
+        public int ValoraPagar { get; set; }
+
+        public string Barrio { get; set; } = null!;
+
+        public string DescripcionTutoria { get; set; } = null!;
+
+    }
+}
