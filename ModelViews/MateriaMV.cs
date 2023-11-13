208d381d69da174fea87b00ab37bf51005df8748
Andrmel1 <141949817+Andrmel1@users.noreply.github.com>
2023-11-13 17:33:35 -0500

    Se actualizan controller de Tutor, Tutor Materia y Materia

---
+++ ModelViews/MateriaMV.cs
@@ -0,0 +1,15 @@
+﻿namespace T5S.ModelsVie
+{
+    public class MateriaMV
+    {
+        public int Id { get; set; }
+
+        public string NombreMateria { get; set; } = null!;
+
+        public string CostoMateria { get; set; } = null!;
+
+        public string PruebaMateria { get; set; } = null!;
+
+        public string Estado { get; set; } = null!;
+    }
+}
---
+++ ModelViews/TutorMV.cs
@@ -0,0 +1,24 @@
+﻿using T5S.Models;
+
+namespace T5S.MoldelsView
+{
+    public class TutorMV
+    {
+        public int IdTutor { get; set; }
+
+        public string NombreTutor { get; set; } = null!;
+
+        public string ApellidoTutor { get; set; } = null!;
+
+        public int NumeroDocumentoTutor { get; set; }
+
+        public string CorreoTutor { get; set; } = null!;
+
+        public string NombreMateria { get; set; } = null!;
+
+        public string CostoMateria { get; set; } = null!;
+
+        public string Estado { get; set; } = null!;
+
+    }
+}
